68f08ce30a0429b457a2298f92062d6e7ff1714d
Raggy <raggym833@gmail.com>
2020-03-15 02:25:34 +0900

    Added comments to PlayerControl.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
-
-
 public class GameManager : MonoBehaviour {
 
     // オーディオの定義
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,59 +6,76 @@ using UnityEngine.EventSystems;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerControl : MonoBehaviour {
 
-    public delegate void PlayerDelegate();
+	// コンストの定義
+	private const float TAP_FORCE = 10f;
+	private const float TILT_SMOOTH = 5f;
+	private const bool SIMULATION_ON = true;
+	private const bool SIMULATION_OFF = false;
+	private const int RANDOM_NUM_MIN = 1;
+	private const int RANDOM_NUM_MAX = 5;
+
+	// デリゲートの定義
+	public delegate void PlayerDelegate();
     public static event PlayerDelegate OnPlayerDied;
     public static event PlayerDelegate OnPlayerScored;
 
-    public float tapForce = 10f;
-    public float tiltSmooth = 5f;
-    public Vector3 startPos;
-
+	// オーディオの定義
     public AudioSource flapAudio;
     public AudioSource dieAudio1, dieAudio2, dieAudio3, dieAudio4;
 
+	// 設定の定義
     Rigidbody2D rb;
     Quaternion downRotation;
     Quaternion forwardRotation;
-
     GameManager gameManager;
 
-    private void Start() {
-        rb = GetComponent<Rigidbody2D>();
-        downRotation = Quaternion.Euler(0, 0, -90);
-        forwardRotation = Quaternion.Euler(0, 0, 35);
-        gameManager = GameManager.Instance;
-        rb.simulated = false;
+	// キャラの設定
+	public float tapForce = TAP_FORCE;
+	public float tiltSmooth = TILT_SMOOTH;
+	public Vector3 startPos;
+
+	// Start
+	private void Start() {
+        rb = GetComponent<Rigidbody2D>();								// Rigidbody2D を取得
+        downRotation = Quaternion.Euler(0, 0, -90);						// 下に向く角度を設定
+        forwardRotation = Quaternion.Euler(0, 0, 35);					// 角度を設定
+        gameManager = GameManager.Instance;								// GameManagerのインスタンスを設定
+        rb.simulated = SIMULATION_OFF;									// Simulation をオフにする
     }
 
+	// OnEnable時の処理
     private void OnEnable() {
-        GameManager.OnGameStarted += OnGameStarted;
-        GameManager.OnGameOverConfirmed += OnGameOverConfirmed;
-        GameManager.BackToMenu += BackToMenu;
-    }
-
+        GameManager.OnGameStarted += OnGameStarted;						// Delegateを足す
+        GameManager.OnGameOverConfirmed += OnGameOverConfirmed;			// Delegateを足す
+		GameManager.BackToMenu += BackToMenu;							// Delegateを足す
+	}
+	
+	// OnDisable時の処理
     private void OnDisable() {
-        GameManager.OnGameStarted -= OnGameStarted;
-        GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
-        GameManager.BackToMenu -= BackToMenu;
+        GameManager.OnGameStarted -= OnGameStarted;						// Delegateを引く
+        GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;			// Delegateを引く
+        GameManager.BackToMenu -= BackToMenu;							// Delegateを引く
     }
 
+	// GameStart時の処理
     void OnGameStarted() {
-        rb.velocity = Vector3.zero;
-        rb.simulated = true;
+        rb.velocity = Vector3.zero;										// 重力をzeroにする
+        rb.simulated = SIMULATION_ON;									// Simulationをオンにする
     }
+
+	// GameOverConfirmed時の処理
     void OnGameOverConfirmed() {
-        transform.localPosition = startPos;
-        transform.rotation = Quaternion.identity;
+        transform.localPosition = startPos;								// ポジションを設定する
+        transform.rotation = Quaternion.identity;						// 角度を設定する
     }
 
+	// BackToMenu時の処理
     void BackToMenu() {
-        rb.simulated = false;
+        rb.simulated = SIMULATION_OFF;									// Simulationをオフにする
     }
 
+	// 画面タッチがUI上なのか確認する処理
     private bool IsPointerOverUIObject() {
-        // TODO
-        // Check how it works
         PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
         eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         List<RaycastResult> results = new List<RaycastResult>();
@@ -66,38 +83,42 @@ public class PlayerControl : MonoBehaviour {
         return results.Count > 0;
     }
 
+	// Update
     private void Update() {
+		// GameOverの場合、処理はやめる
         if (gameManager.GameOver) return;
 
+		// 画面タッチした場合
         if (Input.GetMouseButtonDown(0)) {
-            // Add eventSystem to detect UI click
+			// タッチがUI上の場合
             if (EventSystem.current.IsPointerOverGameObject() || IsPointerOverUIObject())
                 return;
 
-            flapAudio.Play();
-            transform.rotation = forwardRotation;
-            // To restart the velocity
-            rb.velocity = Vector3.zero;
-            rb.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+            flapAudio.Play();												// Audioを再生する
+            transform.rotation = forwardRotation;							// 角度を設定する
+            rb.velocity = Vector3.zero;										// 速度ベクトルの設定
+            rb.AddForce(Vector2.up * tapForce, ForceMode2D.Force);			// X 方面に力を与える
 
         }
+		// 徐々にキャラの角度を下げる処理
         transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
     }
 
+	// OnTriggerEnter2D時の処理
     private void OnTriggerEnter2D(Collider2D col) {
+		// ScoreZoneに当たった場合
         if(col.gameObject.tag == "ScoreZone") {
-            // Add score
             OnPlayerScored(); // event sent to gameManager
-            // Play sound
-        }
+			/* スコア処理を加える予定 */
+			/* SEを足す予定 */
+		}
 
-        if(col.gameObject.tag == "DeadZone") {
-            rb.simulated = false;
-            // dead event
-            OnPlayerDied(); // event sent to gameManager
-            // Create random num and play dieAudio
-            int ran = Random.Range(1, 5);
-            Debug.Log(ran);
+		// DeadZoneに当たった場合
+		if (col.gameObject.tag == "DeadZone") {
+            rb.simulated = SIMULATION_OFF;									// simulationをオフにする
+            OnPlayerDied();													// イベントを gameManager に送る
+            int ran = Random.Range(RANDOM_NUM_MIN, RANDOM_NUM_MAX);			// ランダムな数字を生成する
+			// ranによってランダムなSEを流す
             switch (ran) {
                 case 1:
                     dieAudio1.Play();
