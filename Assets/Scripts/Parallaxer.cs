ffe467d558938078cf05dc38d444c63246c1b1b1
Raggy <raggym833@gmail.com>
2020-04-12 13:09:56 +0900

    Unityのバージョン2018.4.20f1(LTS)にアップデート Parallaxer.csの修正とコメントの足し

--- Assets/Scripts/Parallaxer.cs
+++ Assets/Scripts/Parallaxer.cs
@@ -4,135 +4,135 @@ using UnityEngine;
 
 public class Parallaxer : MonoBehaviour {
 
-    // We are not going to be instantiating or destroying
-    // so we determine the number of objects and
-    // see if they are in use or not
+	// 途中にインスタンスの作成と削除はせずに、最初に生成したもので管理する
 
+	// To keep track of the same type
+	// To check if an object is in use or not
 	class PoolObject {
-        public Transform transform;
-        public bool inUse;
-        public PoolObject(Transform t) { transform = t; }
-        public void Use() { inUse = true; }
-        public void Dispose() { inUse = false; }
+		public Transform transform;
+		public bool inUse;                                          // To determine if it is in use or not
+		public PoolObject(Transform t) { transform = t; }           // Constructor
+		public void Use() { inUse = true; }                         // inUseをtrueにする
+		public void Dispose() { inUse = false; }                    // inUseをfalseにする
+	}
 
-    }
-    [System.Serializable]
-    public struct YSpawnRange {
-        public float min;
-        public float max;
-    }
+	[System.Serializable]
+	public struct YSpawnRange {                                     // パイプのYポジション
+		public float min;                                           // 最小のYポジション
+		public float max;                                           // 最大のYポジション
+	}
 
-    public GameObject Prefab;
-    public int poolSize;
-    public float shiftSpeed;
-    public float spawnRate;
+	public GameObject Prefab;                                       // 生成するオブジェクトのタイプ
+	public int poolSize;                                            // 最初に作成するオブジェクトの数
+	public float shiftSpeed;                                        // 左右に移動するスピード
+	public float spawnRate;                                         // 移動（生成）する比率
 
-    public YSpawnRange ySpawnRange;
-    public Vector3 defaultSpawnPos;
-    public bool spawnImmediate; // particle prewarm
-    public Vector3 immediateSpawnPos;
-    public Vector2 targetAspectRatio;
+	public YSpawnRange ySpawnRange;                                 // ySpawnRangeのレファレンス
+	public Vector3 defaultSpawnPos;                                 // デフォルトで生成されるポジション
+	public bool spawnImmediate;                                     // Prewarmの有無
+	public Vector3 immediateSpawnPos;                               // Prewarmの場合のポジション
+	public Vector2 targetAspectRatio;                               // 画面内で生成されないように画面の情報を管理する
 
-    float spawnTimer;
-    float targetAspect;
-    PoolObject[] poolObjects;
-    GameManager gameManager;
+	float spawnTimer;                                               // 
+	float targetAspect;                                             // 
+	PoolObject[] poolObjects;                                       // poolObjectsのレファレンス
+	GameManager gameManager;                                        // gameManagerのレファレンス
 
-    private void Awake() {
-        Configure();
-    }
+	private void Awake() {
+		Configure();                                                // 
+	}
 
-    private void Start() {
-        gameManager = GameManager.Instance;
+	private void Start() {                                          // GameManagerはAwakeで作成されているので、Startで設定すればnullでないことが保証される
+		gameManager = GameManager.Instance;                         // GameManagerのインスタンスを
 
-    }
+	}
 
-    private void OnEnable() {
-        GameManager.OnGameOverConfirmed += OnGameOverConfirmed;
-    }
+	private void OnEnable() {
+		GameManager.OnGameOverConfirmed += OnGameOverConfirmed;     // サブスクライブに足す
+	}
 
-    private void OnDisable() {
-        GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
-    }
+	private void OnDisable() {
+		GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;     // サブスクライブから外す
+	}
 
-    void OnGameOverConfirmed() {
-        for (int i = 0; i < poolObjects.Length; i++) {
-            poolObjects[i].Dispose();
-            poolObjects[i].transform.position = Vector3.one * 1000;
-        }
-        if (spawnImmediate) {
-            SpawnImmediate();
-        }
-    }
+	void OnGameOverConfirmed() {
+		for (int i = 0; i < poolObjects.Length; i++) {                          // poolObjectをループする
+			poolObjects[i].Dispose();                                           // オブジェクトにDisposeを呼ぶ
+			poolObjects[i].transform.position = Vector3.one * 1000;             // 一時的に画面外に移動させる
+		}
+		if (spawnImmediate) {                                                   // 
+			SpawnImmediate();                                                   // 
+		}
+	}
 
-    private void Update() {
-        if (gameManager.GameOver) return;
-        Shift();
-        spawnTimer += Time.deltaTime;
-        if(spawnTimer > spawnRate) {
-            Spawn();
-            spawnTimer = 0;
-        }
-    }
+	private void Update() {
+		if (gameManager.GameOver) return;                                       // GameOverの場合returnする
+		Shift();                                                                // Shiftを呼ぶ
+		spawnTimer += Time.deltaTime;                                           // spawnTimerをカウントアップする
+		if (spawnTimer > spawnRate) {                                           // spawnTimerがspawnRateより大きい場合
+			Spawn();                                                            // Spawnを呼ぶ
+			spawnTimer = 0;                                                     // spawnTimerをリセットする
+		}
+	}
 
-    void Configure() {
-        targetAspect = targetAspectRatio.x / targetAspectRatio.y;
-        poolObjects = new PoolObject[poolSize];
-        for (int i = 0; i < poolObjects.Length; i++) {
-            GameObject go = Instantiate(Prefab) as GameObject;
-            Transform t = go.transform;
-            t.SetParent(transform);
-            t.position = Vector3.one * 1000;
-            poolObjects[i] = new PoolObject(t);
-        }
-        if (spawnImmediate) {
-            SpawnImmediate();
-        }
-    }
+	void Configure() {
+		targetAspect = targetAspectRatio.x / targetAspectRatio.y;               // 縦横の比率を確認
+		poolObjects = new PoolObject[poolSize];                                 // poolObjectのインスタンスを作成
+		for (int i = 0; i < poolObjects.Length; i++) {                          // poolObjectsをループする
+			GameObject gameobject = Instantiate(Prefab) as GameObject;          // Gameobjectをinstantiateする
+			Transform t = gameobject.transform;                                 // Transformを取得する
+			t.SetParent(transform);                                             // このスクリプトを保持しているParentオブジェクトを設定する
+			t.position = Vector3.one * 1000;                                    // 一時的に画面外に保持する
+			poolObjects[i] = new PoolObject(t);                                 // 設定された情報をオブジェクトに設定する
+		}
+		if (spawnImmediate) {                                                   // 
+			SpawnImmediate();                                                   // 
+		}
+	}
 
-    void Spawn() {
-        Transform t = GetPoolObject();
-        if (t == null) return; // if true, this indicated that poolSize is too small
-        Vector3 pos = Vector3.zero;
-        pos.x = (defaultSpawnPos.x * Camera.main.aspect) / targetAspect;
-        pos.y = Random.Range(ySpawnRange.min, ySpawnRange.max);
-        t.position = pos;
-    }
+	void Spawn() {
+		Transform t = GetPoolObject();                                          // GetPoolObjectでreturnされる使用可能なobjectを保持する
+		if (t == null) return;                                                  // nullチェックを行う
+		Vector3 pos = Vector3.zero;                                             // posを初期化する
+		pos.x = (defaultSpawnPos.x * Camera.main.aspect) / targetAspect;        // x posを設定する
+		pos.y = Random.Range(ySpawnRange.min, ySpawnRange.max);                 // y posを設定する
+		t.position = pos;                                                       // posをオブジェクトのポジションに設定する
+	}
 
-    void SpawnImmediate() {
-        Transform t = GetPoolObject();
-        if (t == null) return; // if true, this indicated that poolSize is too small
-        Vector3 pos = Vector3.zero;
-        pos.x = (immediateSpawnPos.x * Camera.main.aspect) / targetAspect;
-        pos.y = Random.Range(ySpawnRange.min, ySpawnRange.max);
-        t.position = pos;
-        Spawn();
-    }
+	void SpawnImmediate() {
+		Transform t = GetPoolObject();                                          // GetPoolObjectでreturnされる使用可能なobjectを保持する
+		if (t == null) return;                                                  // nullチェックを行う
+		Vector3 pos = Vector3.zero;                                             // posを初期化する
+		pos.x = (immediateSpawnPos.x * Camera.main.aspect) / targetAspect;      // x posを設定する
+		pos.y = Random.Range(ySpawnRange.min, ySpawnRange.max);                 // y posを設定する
+		t.position = pos;                                                       // posをオブジェクトのポジションに設定する
+		Spawn();                                                                // 
+	}
 
-    void Shift() {
-        for (int i = 0; i < poolObjects.Length; i++) {
-            poolObjects[i].transform.position += Vector3.left * shiftSpeed * Time.deltaTime;
-            CheckDisposeObject(poolObjects[i]);
-        }
-    }
+	void Shift() {                                                                                              // オブジェクトを移動させる
+		for (int i = 0; i < poolObjects.Length; i++) {                                                          // poolObjectをループする
+			poolObjects[i].transform.position += Vector3.left * shiftSpeed * Time.deltaTime;                    // shiftSpeedで移動させる
+			CheckDisposeObject(poolObjects[i]);                                                                 // CheckDisposeObjectを呼ぶ
+		}
+	}
 
-    void CheckDisposeObject(PoolObject poolObject) {
-        if(poolObject.transform.position.x < (-defaultSpawnPos.x * Camera.main.aspect) / targetAspect) {
-            System.Console.WriteLine(poolObject.transform.position.x);
-            System.Console.WriteLine(-defaultSpawnPos.x);
-            poolObject.Dispose();
-            poolObject.transform.position = Vector3.one * 1000;
-        }
-    }
+	void CheckDisposeObject(PoolObject poolObject) {                                                            // 特定のオブジェクトを削除する
+		if (poolObject.transform.position.x < (-defaultSpawnPos.x * Camera.main.aspect) / targetAspect) {       // オブジェクトが画面外にいる場合
+			System.Console.WriteLine(poolObject.transform.position.x);
+			System.Console.WriteLine(-defaultSpawnPos.x);
+			poolObject.Dispose();                                                                               // Disposeを呼ぶ
+			poolObject.transform.position = Vector3.one * 1000;                                                 // 再度使用されるまで一時的に画面外に保持する
+		}
+	}
 
-    Transform GetPoolObject(){
-        for (int i = 0; i < poolObjects.Length; i++) {
-            if (!poolObjects[i].inUse) {
-                poolObjects[i].Use();
-                return poolObjects[i].transform;
-            }
-        }
-        return null;
-    }
+	Transform GetPoolObject() {                                                 // 特定のオブジェクトを取得する
+		for (int i = 0; i < poolObjects.Length; i++) {                          // poolObjectsをループする
+			if (!poolObjects[i].inUse) {                                        // オブジェクトがinUseでなかった場合
+				poolObjects[i].Use();                                           // オブジェクトをinUseに変換
+				return poolObjects[i].transform;                                // オブジェクトをreturnする
+			}
+		}
+		return null;
+	}
 
 }
